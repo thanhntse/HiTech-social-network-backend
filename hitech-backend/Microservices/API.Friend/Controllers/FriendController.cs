472795ce6c22322d0f012b0cbd344a855e9fb51d
thanhntse <nguyenthaithanh07@gmail.com>
2024-07-27 00:20:52 +0700

    refactor: controller of 3 svc

---
+++ hitech-backend/Microservices/API.Friend/Controllers/FriendController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Friend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class FriendController : ControllerBase
+    {
+    }
+}
