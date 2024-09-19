5af5731d71553f361e36b8a356d88d8a4dd56db0
thanhntse <nguyenthaithanh07@gmail.com>
2024-09-19 21:21:39 +0700

    feat: apply api resp pattern

---
+++ Microservices/HiTech.Service.AccountAPI/Controllers/AccountsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HiTech.Service.AccountAPI.Data;
+using HiTech.Service.AccountAPI.Entities;
+using HiTech.Service.AccountAPI.Services;
+using HiTech.Service.AccountAPI.DTOs.Response;
+using Microsoft.AspNetCore.Http.HttpResults;
+using HiTech.Shared.Controllers;
+using Azure;
+using HiTech.Service.AccountAPI.DTOs.Request;
+using Azure.Core;
+
+namespace HiTech.Service.AccountAPI.Controllers
+{
+    [Route("api/hitech/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountsController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        // GET: api/hitech/accounts
+        [HttpGet]
+        public ActionResult<ApiResponse<IAsyncEnumerable<AccountResponse>>> GetAccounts()
+        {
+            var accounts = _accountService.GetAllAsync();
+            var response = HiTechApi.ResponseOk(accounts);
+            return Ok(response);
+        }
+
+        // GET: api/hitech/accounts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApiResponse<AccountResponse>>> GetAccount(int id)
+        {
+            var account = await _accountService.GetByIDAsync(id);
+
+            var response = account != null
+                ? HiTechApi.ResponseOk(account)
+                : HiTechApi.ResponseNotFound<AccountResponse>();
+
+            return account != null ? Ok(response) : NotFound(response);
+        }
+
+        // PUT: api/hitech/accounts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ApiResponse<object>>> PutAccount(int id, AccountRequest request)
+        {
+            var response = HiTechApi.ResponseNotFound<object>();
+
+            if (!await _accountService.AccountExists(id))
+            {
+                return NotFound(response);
+            }
+
+            bool flag = await _accountService.UpdateAsync(id, request);
+
+            if (flag)
+            {
+                return NoContent();
+            }
+            response = HiTechApi.ResponseBadRequest<object>();
+            return BadRequest(response);
+        }
+
+        // POST: api/hitech/accounts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse<AccountResponse>>> PostAccount(AccountRequest request)
+        {
+            var response = HiTechApi.ResponseConflict<AccountResponse>();
+            if (await _accountService.AccountExists(request.Email))
+            {
+                return Conflict(response);
+            }
+
+            var account = await _accountService.CreateAsync(request);
+
+            if (account == null)
+            {
+                response = HiTechApi.ResponseBadRequest<AccountResponse>();
+                return BadRequest(response);
+            }
+
+            response = HiTechApi.Response(201, "Created", account);
+            return CreatedAtAction("GetAccount", response);
+        }
+
+        // DELETE: api/hitech/accounts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ApiResponse<object>>> DeleteAccount(int id)
+        {
+            var response = HiTechApi.ResponseNotFound<object>();
+
+            if (!await _accountService.AccountExists(id))
+            {
+                return NotFound(response);
+            }
+
+            bool flag = await _accountService.DeleteAsync(id);
+            if (flag)
+            {
+                return NoContent();
+            }
+            response = HiTechApi.ResponseBadRequest<object>();
+            return BadRequest(response);
+        }
+    }
+}
---
+++ Shared/HiTech.Shared/Controllers/ApiResponse.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HiTech.Shared.Controllers;
+
+/// <summary>
+/// Response to an API call.
+/// </summary>
+public class ApiResponse<T>
+{
+    /// <summary>
+    /// Status of the API call, following HTTP status codes convention.
+    /// </summary>
+    [JsonPropertyName("status")]
+    public int Status { get; set; }
+
+    /// <summary>
+    /// Extra information if any (e.g. the detailed error message).
+    /// </summary>
+    [JsonPropertyName("message")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Message { get; set; }
+
+    /// <summary>
+    /// The data returned by the API call (specific to individual API).
+    /// </summary>
+    [JsonPropertyName("data")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public T? Data { get; set; }
+
+    /// <summary>
+    /// Extra data if any.
+    /// </summary>
+    [JsonPropertyName("extras")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public object? Extras { get; set; }
+
+    /// <summary>
+    /// Debug information if any.
+    /// </summary>
+    [JsonPropertyName("debug_info")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public object? DebugInfo { get; set; }
+}
